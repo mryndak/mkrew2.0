9acd4cba66bfe41a497a162b4086428716746c8f
Mateusz Ryndak <kontakt@mryndak.pl>
2025-10-03 20:14:25 +0200

    Initialize project structure for scraper with Gradle and Spring Boot configuration

---
+++ scraper/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.axion.release)
+}
+
+group = "pl.mkrew"
+version = scmVersion.version
+description = "Demo project for Spring Boot"
+
+scmVersion {
+	tag {
+		prefix.set("v")
+	}
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.spring.boot.starter.data.jpa)
+	implementation(libs.spring.boot.starter.quartz)
+	implementation(libs.jsoup)
+	implementation(libs.liquibase.core)
+	runtimeOnly(libs.postgresql)
+	testImplementation(libs.spring.boot.starter.test)
+	testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ scraper/src/test/java/pl/mkrew/scraper/ScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkrew.scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
